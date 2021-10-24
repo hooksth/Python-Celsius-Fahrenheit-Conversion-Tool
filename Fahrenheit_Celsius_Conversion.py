07388d78ab9ab2961f5d24b611db1bc1ccc23894
hooksth <93074106+hooksth@users.noreply.github.com>
2021-10-24 05:25:56 -0400

    Delete Fahrenheit_Celsius_Conversion.py

--- Fahrenheit_Celsius_Conversion.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("Weight: "))
-unit = input("(K)g or (L)bs: ")
-if unit.upper() == "K":
-    convert = weight / 0.45
-    print("Your weight in lbs is " + str(convert))
-else:
-    convert = weight * 0.45
-    print("Your weight in Kg is " + str(convert))
